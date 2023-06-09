2e3aa611770db0e4ed54efc8fffbf564b44ae575
S-Lombardi <silo.lombardi@gmail.com>
2023-06-09 16:18:57 +0200

    Realizzato un markup statico (brutto) di base. Aggiunto array lista inizialmente con soli 3 oggetti. Uso del ciclo v-for per stamparlia video

--- js/script.js
+++ js/script.js
@@ -2,13 +2,31 @@ const { createApp } = Vue;
 
 createApp({
     data(){
-        return
-
+        return{
+            list:[
+                {
+                    text:'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text:'Andare in bici',
+                    done: false,
+                },
+                {
+                    text:'Studiare',
+                    done: false,
+                },
+            ]
+        }
+       
     },
+
+    indice: 0,
+
     methods:{
 
         
-    }
+    },
 
 
-}).mount('#app')
+}).mount('#app');
